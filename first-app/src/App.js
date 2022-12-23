ec3a8506cb8da53413dfa6b8b3360cc4050c0685
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-12-23 22:49:57 +0100

    update

---
+++ first-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Props from './components/Props';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Salam</h1>
+      <Props Fn="Yassine" Ls="benmansour" Gp="Deb 109" Nm="2" />
+    </div>
+  );
+}
+
+export default App;
---
+++ first-app/src/components/Props.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Props({Fn , Ls , Gp, Nm}) {
+  return (
+    <div>
+      <h1>hello my full name is  {Fn} {Ls} as well as my groupe is :{Gp} and my rank is :{Nm} </h1>
+    </div>
+  )
+}
