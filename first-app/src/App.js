3207157a7c3803f331c29ad8d30a0191ed9fe1c3
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-12-24 17:54:01 +0100

    update

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import Props from './components/Props';
-import Stateh from './components/Stateh';
-import Compo1 from './components/Compo1';
+import Calcul from './components/Calcul';
 
 function App() {
   return (
     <div className="App container m-5">
-      
-      <h1 className='text-primary '>Salam</h1>
-      <Props Fn="Yassine" Ls="benmansour" Gp="Deb 109" Nm="2" />
-
-      <Stateh />
-
-      <br />
-      <Compo1 />
+      <Calcul />
 
     </div>
   );
