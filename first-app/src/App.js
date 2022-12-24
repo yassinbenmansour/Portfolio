939e6a28d57806dbc69134a83b6f58ef02509581
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-12-24 01:44:37 +0100

    update

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -3,9 +3,9 @@ import Props from './components/Props';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container m-5">
       
-      <h1>Salam</h1>
+      <h1 className='text-primary '>Salam</h1>
       <Props Fn="Yassine" Ls="benmansour" Gp="Deb 109" Nm="2" />
     </div>
   );
