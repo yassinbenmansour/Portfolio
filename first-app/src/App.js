2ae16c9d4a229dda445a63c6ad7a14c74d646323
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2022-12-24 15:08:43 +0100

    update

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Props from './components/Props';
+import Stateh from './components/Stateh';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       
       <h1 className='text-primary '>Salam</h1>
       <Props Fn="Yassine" Ls="benmansour" Gp="Deb 109" Nm="2" />
+
+      <Stateh />
     </div>
   );
 }
