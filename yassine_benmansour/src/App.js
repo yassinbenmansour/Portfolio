a8262b5b688d2b9e3c682ef595dfcfba482e2fad
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-01-11 04:17:57 +0100

    update

---
+++ yassine_benmansour/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Button from '@mui/material/Button';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello World</Button>
+      <Button variant="text">Text</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+}
+
+export default App;
